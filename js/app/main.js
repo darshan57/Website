08d528c22739d2f81fb175d6957a6896d42b9392
Darshan Gowda <Darshan Gowda>
2017-07-05 23:06:54 +0530

    initial commit

---
+++ js/app/main.js
@@ -0,0 +1,36 @@
+require.config({
+  shim: {   // Using shim we define dependencies between our libraries
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: [
+        'underscore',
+        'jquery'
+      ],
+      exports: 'Backbone'
+    },
+    'material':{
+        deps:[
+            'jquery'
+        ],
+        exports:'Material'
+    }
+  },
+  paths: {   // Require.js allows us to configure shortcut alias
+    jquery: '../../libs/jquery/jquery-min',
+    underscore: '../../libs/underscore/underscore-min',
+    backbone: '../../libs/backbone/backbone-min',
+    text: '../../libs/require/text',
+    templates: 'templates',
+    material: '../../libs/material-css/js/materialize.min' 
+  }
+});
+
+require([
+  // Load our app module and pass it to our definition function
+  'app',
+
+], function(App){ // The "app" dependency is passed in as "App"
+  App.initialize();
+});
